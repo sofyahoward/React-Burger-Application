a9b83b8bcfcb81f5d3f5cd8fb16dba0c94a36173
sofyahoward <sofya@vt.edu>
2018-04-17 14:25:05 -0400

    created a burger, burger builder and layout components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from "./Components/Layout/Layout";
+import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder"
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
     );
   }
